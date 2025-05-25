c1295294a6b50d24569d8e7cb1c63fd6073fc795
Dan Richert <dan.richert@gmail.com>
2025-05-25 09:54:36 -0400

    fix form alignment

--- src/ControlSettings.tsx
+++ src/ControlSettings.tsx
@@ -3,13 +3,13 @@ import ParamMap from './lib/PocketC/ParamMap.ts'
 const ControlSettings = function ({ controlNumber }) {
 	return (
 		<form>
-			<div className="form-group">
-				<label htmlFor="number">Control Number</label>
-				<input type="text" value={controlNumber} />
+			<div className="mb-3">
+				<label htmlFor="number" className="form-label">Control Number</label>
+				<input className="form-control" type="text" value={controlNumber} />
 			</div>
 
-			<div className="form-group">
-				<label htmlFor="channel">Channel</label>
+			<div className="mb-3">
+				<label htmlFor="channel" className="form-label">Channel</label>
 				<input id="channel"
 					type="number"
 					className="form-control"
@@ -17,9 +17,9 @@ const ControlSettings = function ({ controlNumber }) {
 				/>
 			</div>
 
-			<div className="form-group">
-				<label htmlFor="param">Parameter</label>
-				<select id="param">
+			<div className="mb-3">
+				<label htmlFor="param" className="">Parameter</label>
+				<select id="param" className="form-control">
 					{Object.entries(ParamMap).map(([k, v]) => (
 						<option key={k} value={k}>{`${k}: ${v}`}</option>
 					))}
